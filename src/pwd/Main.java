c3c23d5da03f3770f1f2d6bc1007c792f4b77b75
QOSMIO <QOSMIO@QOSMIO-PC>
2015-03-18 20:31:53 -0300

    Versao original

---
+++ src/pwd/Main.java
@@ -0,0 +1,14 @@
+package pwd;
+
+/**
+ * Test password level
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		String passwd = "2hAj5#mne-ix.86H";
+		System.out.println(CheckStrength.checkPasswordStrength(passwd));
+	}
+
+}
